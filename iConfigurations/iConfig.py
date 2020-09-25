088348252bba206323e81e77d89ba3c81df67317
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2020-09-25 19:48:09 +0100

    widgets configurations

---
+++ iConfigurations/iConfig.py
@@ -0,0 +1,12 @@
+'''
+    Genric configurations for all widgets
+'''
+
+generics = {
+    'padding': ('5px', '5px', '5px', '5px'),
+    'margin': ('5px', '5px', '5px', '5px'),
+    'borderRadius': ('5px', '5px', '5px', '5px'),
+    'border': ('5px', '5px', '5px', '5px'),
+    'color': 'white',
+    'backgroundColor': '#333'
+}
