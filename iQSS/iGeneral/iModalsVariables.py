8e38049904b558515f46fdbd18881de3b54c565b
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2020-10-13 23:05:31 +0100

    iGenerals : Modal

---
+++ iQSS/iGeneral/iModalsVariables.py
@@ -0,0 +1,14 @@
+variables = {
+    'background-color': '#ffffff',
+    'color': 'white',
+    'border-width': '0',
+    'border-style': 'solid',
+    'border-color': 'rgba(0, 0, 0, 0.125)',
+    'border-radius': '0.25rem',
+    'margin': '0',
+    'min-width': '40px',
+    'min-height': '30px',
+    'font-family': 'Verdana',
+    'padding': '0.5rem 0.5rem',
+    'text-align': 'center',
+}
